30e19769f5e8e335013b03bb843d21521b9b0990
Zephyr Mann <zephmann@gmail.com>
2021-04-13 20:31:00 -0400

    First pass at convert function.

---
+++ roman_numerals.py
@@ -0,0 +1,49 @@
+# :coding: utf-8
+
+
+NUMERAL_CHARACTERS = [
+    "I",  # one
+    "V",  # five
+    "X",  # ten
+    "L",  # fifty
+    "C",  # hundred
+    "D",  # five hundred
+    "M",  # thousand
+]
+
+
+def convert(value: int) -> str:
+    """Convert an integer *value* into a string of Roman numerals."""
+    # TODO validate input
+
+    digits = map(int, reversed(list(str(value))))
+    
+    numerals = ""
+    for exp, digit in enumerate(digits):
+        # skip any zeros
+        if not digit:
+            continue
+
+        numerals = _convert_digit(digit, exp) + numerals
+
+    return numerals
+
+
+def _convert_digit(digit: int, exp: int) -> str:
+    """Helper function to convert a single digit."""
+    char_offset = exp * 2
+    ones = NUMERAL_CHARACTERS[char_offset]
+    fives = NUMERAL_CHARACTERS[char_offset + 1]
+    tens = NUMERAL_CHARACTERS[char_offset + 2]
+
+    if digit == 9:
+        return f"{ones}{tens}"
+
+    if digit == 4:
+        return f"{ones}{fives}"
+
+    numerals = f"{fives}" if digit > 4 else ""
+
+    numerals += f"{ones}" * (digit % 5)
+
+    return numerals
